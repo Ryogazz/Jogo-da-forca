9c2fcede7c335afb803e7c663a533a3b51b4472c
Ryogazz <josuex.x.x@hotmail.com>
2020-12-12 18:51:17 -0300

    jogo da forca

---
+++ Jogo da forca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogo_da_forca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
