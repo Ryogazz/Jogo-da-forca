0d11aeb9b4cacbc7eab768aeb3e4ef34677c679a
Ryogazz <josuex.x.x@hotmail.com>
2020-12-14 20:32:06 -0300

    implementando codigo do jogo da forca

--- Jogo da forca/Program.cs
+++ Jogo da forca/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Jogo_da_forca
 {
@@ -6,7 +7,78 @@ namespace Jogo_da_forca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] palavras = { "ar", "gas", "casa", "futebol" };
+            //amazena a palavra e a letra selecionada
+            string palavra = "", letra = "";
+
+            int erros = 0, completo = 0, posicao = 0;
+            //bool para manter o loop do game
+            bool sair = false;
+            const int Limite = 7;
+            Random rnd = new Random();
+
+            //escolha vai receber um valor de 0 a 3 que esse valor sera definido para que palavra receba 
+            //uma das palavras que esta associada no endereço do vetor palavras
+            int escolha = rnd.Next(0, 3);
+            palavra = palavras[escolha];
+            //aqui usamos Length, palavra vai armazenar o numero de caracteres que a palavra te
+            string[] quebrada = new string[palavra.Length];
+
+            while (!sair)
+            {
+                Console.Clear();
+                Console.WriteLine("Erros: " + erros + " de " + Limite);
+
+                for (int i = 0; i < quebrada.Length; i++)
+                {
+                    if (quebrada[i] != null)
+                    {
+                        Console.Write(quebrada[i] + " ");
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+                    Console.WriteLine("\nEscolha a posição da letra!");
+                    posicao = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a letra: ");
+                    letra = Console.ReadLine();
+
+                    //verificando se a letra na posiçao digitaa corresponde a palavra que foi selecionada.
+
+                    if (palavra.ElementAt(posicao - 1) == letra.ElementAt(0))
+                    {
+                        quebrada[posicao - 1] = letra;
+                        completo++;
+                    }
+                    else
+                    {
+                        erros++;
+                    }
+
+                    if (erros >= Limite)
+                    {
+                        sair = true;
+                    }
+
+                    if (completo == palavra.Length)
+                    {
+                        sair = true;
+                    }
+                
+            }
+            if (completo == palavra.Length)
+            {
+                Console.Clear();
+                Console.WriteLine("Parabens voce acertou a palavra " + palavra);
+            }
+            else if (erros == Limite)
+            {
+                Console.Clear();
+                Console.WriteLine("Voce foi enforcado a palavra era " + palavra);
+            }
+            
         }
     }
 }
